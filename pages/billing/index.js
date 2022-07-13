07da57641e6b545df250ec89b6b8f8d94760f4bd
Taha Nemat <tahanemat6@gmail.com>
2022-07-13 18:30:04 +0500

    add tailwindcss, add responsive navbar, add home and billing pages

--- pages/billing/index.js
+++ pages/billing/index.js
@@ -1,23 +1,21 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Navbar from '../components/global/NavBar'
+import Navbar from '../../components/global/NavBar'
 
 export default function Home() {
   return (
-
     <>
       <Head>
-        <title>Guftbot</title>
+        <title>Billing</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar></Navbar>
+      <Navbar />
+
     <div className="flex justify-center items-center flex-col pt-40 text-center font-bold lg:text-8xl text-6xl space-y-2">
-      <h1 className="text-gray-900 pb-10">
-        Guftbot built using <span className="text-blue-500">Next.js</span> &{" "}
-        <span className="text-blue-400">Tailwind</span>
-      </h1>
+        <div className='flex'>
+          <div className='justify-center items-center px-10 py-10 m-10 text-center bg-gray-400 rounded-lg'>Section 1</div>
+          <div className='justify-center items-center px-10 py-10 m-10 text-center bg-gray-400 rounded-lg'>Section 2</div>
+        </div>
       <div className="flex justify-center items-center cursor-pointer hover:shadow-lg  text-3xl font-semibold text-white bg-gray-900 rounded-lg w-64 h-16">
         <h1 className="text-center">{"Add to slack 🚀 "}</h1>
       </div>
