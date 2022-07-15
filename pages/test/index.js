4ee56bf92854816345a178d9ef8951ffce5e612b
Taha Nemat <tahanemat6@gmail.com>
2022-07-15 17:46:06 +0500

    update login button

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,31 +1,17 @@
 import { useSession, signIn, signOut } from "next-auth/react"
+import LoginButton from "../../components/login-btn"
 
 export default function Component({myId, otherId}) {
-    const { data: session } = useSession()
-
-    if (session) {
-        return (
-            <>
-                Signed in as {session.user.email} <br />
-                <button onClick={() => signOut()}>Sign out</button>
-            </>
-        )
-    }
     return (
-        <>
-            <div>{myId}</div>
-            <div>{otherId}</div>
-            <div>Not signed in</div>
-            <button onClick={() => signIn()}>Sign in</button>
-        </>
+        <LoginButton />
     )
 }
 
-export async function getServerSideProps() {
-    // Fetch data from external API
-    const myId = process.env.SLACK_CLIENT_ID;
-    const otherId = process.env.SLACK_CLIENT_SECRET;
+// export async function getServerSideProps() {
+//     // Fetch data from external API
+//     const myId = process.env.SLACK_CLIENT_ID;
+//     const otherId = process.env.SLACK_CLIENT_SECRET;
 
-    // Pass data to the page via props
-    return { props: { myId, otherId } }
-}
+//     // Pass data to the page via props
+//     return { props: { myId, otherId } }
+// }
