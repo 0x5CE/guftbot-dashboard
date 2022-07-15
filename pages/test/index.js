c4963c17679febfacef0dabe88b423f1c2cde898
Taha Nemat <tahanemat6@gmail.com>
2022-07-15 16:42:26 +0500

    add env vars for testing

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,21 +1,31 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
-export default function Component() {
-  const { data: session } = useSession()
+export default function Component({myId, otherId}) {
+    const { data: session } = useSession()
 
-  if (session) {
+    if (session) {
+        return (
+            <>
+                Signed in as {session.user.email} <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        )
+    }
     return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+        <>
+            <div>{myId}</div>
+            <div>{otherId}</div>
+            <div>Not signed in</div>
+            <button onClick={() => signIn()}>Sign in</button>
+        </>
     )
-  }
-  return (
-    <>
+}
 
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const myId = process.env.SLACK_CLIENT_ID;
+    const otherId = process.env.SLACK_CLIENT_SECRET;
+
+    // Pass data to the page via props
+    return { props: { myId, otherId } }
 }
